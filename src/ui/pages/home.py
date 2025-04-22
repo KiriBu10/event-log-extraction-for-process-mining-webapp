90cbbcea7fb22cda138335082460304a1f1e7632
KiriBu10 <kiranbusch@t-online.de>
2025-04-22 04:55:09 -0700

    text updates

--- src/ui/pages/home.py
+++ src/ui/pages/home.py
@@ -3,7 +3,7 @@ from src.ui.components import app_state, navigation
 
 
 # Header Section with Icon
-st.title("Event Log Extraction for Process Mining Using Large Language Models")
+st.title("From Prompt to Process: Large Language Model-Assisted Event Log Extraction from Relational Databases")
 
 st.markdown("by reasearchers from Utrecht University and KLU""", unsafe_allow_html=True)
 
@@ -15,7 +15,7 @@ _app_state: app_state.AppState = st.session_state[app_state.KEY]
 st.markdown(
     """
         <span style="font-size: 1.5em; font-weight: bold;">P</span>rocess mining is a discipline that enables organizations to discover and analyze their work processes. A prerequisite for conducting a process mining initiative is the so-called event log, which is not always readily available. In such cases, extracting an event log involves various time-consuming tasks, such as creating tailor-made structured query language (SQL) scripts to extract an event log from a relational database. With this work, we investigate the use of large language models (LLMs) to support event log extraction, particularly by leveraging LLMs ability to produce SQL scripts.
-        This web application enables users to upload their own database or choose from sample example database, fostering an intuitive and seamless exploration experience. 
+        This web application is an implementation based on our previous work [1](https://link.springer.com/chapter/10.1007/978-3-031-81375-7_4) and enables users to upload their own database or choose from sample example database, fostering an intuitive and seamless exploration experience. 
         Developed by researchers from <strong>Kühne Logistics University</strong>, this tool serves as a solid foundation for event log generation, allowing users to ask targeted questions and receive real-time responses.
         
         
@@ -24,6 +24,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+st.text("[1] Vinicius Stein Dani, Marcus Dees, Henrik Leopold, Kiran Busch, Iris Beerepoot, Jan Martijn EM van der Werf, Hajo A Reijers (2024) Event Log Extraction for Process Mining Using Large Language Models.")
 
 # Feature Section Header
 st.markdown('CURRENT FUNCTIONALITIES', unsafe_allow_html=True)
