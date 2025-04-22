65372663f08f7fa82c5166f021a29d9c331cf6dc
KiriBu10 <kiranbusch@t-online.de>
2025-04-22 05:02:45 -0700

    text updates

--- src/ui/pages/home.py
+++ src/ui/pages/home.py
@@ -15,10 +15,8 @@ _app_state: app_state.AppState = st.session_state[app_state.KEY]
 st.markdown(
     """
         <span style="font-size: 1.5em; font-weight: bold;">P</span>rocess mining is a discipline that enables organizations to discover and analyze their work processes. A prerequisite for conducting a process mining initiative is the so-called event log, which is not always readily available. In such cases, extracting an event log involves various time-consuming tasks, such as creating tailor-made structured query language (SQL) scripts to extract an event log from a relational database. With this work, we investigate the use of large language models (LLMs) to support event log extraction, particularly by leveraging LLMs ability to produce SQL scripts.
-        This web application is an implementation based on our previous work [1](https://link.springer.com/chapter/10.1007/978-3-031-81375-7_4) and enables users to upload their own database or choose from sample example database, fostering an intuitive and seamless exploration experience. 
+        This web application is an implementation based on our previous work [[1](https://link.springer.com/chapter/10.1007/978-3-031-81375-7_4)] and enables users to upload their own database or choose from sample example database, fostering an intuitive and seamless exploration experience. 
         Developed by researchers from <strong>Kühne Logistics University</strong>, this tool serves as a solid foundation for event log generation, allowing users to ask targeted questions and receive real-time responses.
-        
-        
         <br>
         <br>
     """,
@@ -32,7 +30,7 @@ st.markdown('CURRENT FUNCTIONALITIES', unsafe_allow_html=True)
 # Functionality List with Link Button
 st.markdown(
     """
-    <strong>DB</strong>: Upload or select an database. Users can upload their own database or choose from preloaded example databases to generate event log. The file must be in <code>.zip</code> format to ensure compatibility. See here the file requirements: ....
+    <strong>DB</strong>: Upload or select an database. Users can upload their own database or choose from preloaded example databases to generate event log. The file must be in <code>.zip</code> format to ensure compatibility. See [here](https://github.com/KiriBu10/event-log-extraction-for-process-mining-webapp/blob/dev_kiran/README.md) the file requirements.
     <br>
     <br>
     <strong>Single Question Response</strong>: The chatbot is designed to respond to one question at a time, optimizing for security and clarity in interactions. Due to prompt injection concerns and other security considerations, the chatbot only processes the most recent question, without relying on prior context. This means that each query should be self-contained, as previous context is not retained or considered when generating responses. 
